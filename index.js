a8c9cff959344c89ec8196673ddf96db6eadc00b
shadowolf <shadowolfbusiness@gmail.com>
2018-05-30 11:53:21 +1000

    Switching token.

--- index.js
+++ index.js
@@ -52,5 +52,5 @@ client.on('message', message => {
   if (commandfile) commandfile.run(client,message,args);
 });
 
-//client.login(process.env.BOT_TOKEN);
-client.login(token.token);
+client.login(process.env.BOT_TOKEN);
+//client.login(token.token);

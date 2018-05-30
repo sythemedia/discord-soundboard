31fd731c17887f3fac041aa6f35d1e268684e34c
shadowolf <shadowolfbusiness@gmail.com>
2018-05-30 12:36:37 +1000

    Changed from teamspeak to join.

--- commands/join.js
+++ commands/join.js
@@ -1,16 +1,18 @@
 const Discord = require('discord.js');
 
 module.exports.run = async (client, message, args) => {
-  const voiceChannel = message.member.voiceChannel;
 
   if (!message.member.voiceChannel) {
     message.channel.send('Lol, I am not in a voice channel...');
   } else {
-    voiceChannel.join().then(connection => {
-      const dispatcher = connection.playFile('./effects/teamspeak/connected.mp3');
+    message.member.voiceChannel.join().then(connection => {
+      const sounds = ['blockeduserenteredchannel.mp3', 'blockeduserjoinedchannel.mp3', 'buddyenteredchannel.mp3', 'buddyjoinedchannel.mp3', 'connected.mp3', 'userenteredchannel.mp3', 'userjoinedchannel.mp3', 'welcomeback.mp3'];
+      const play = sounds[Math.floor(Math.random() * sounds.length)];
 
-      dispatcher.on("end", end => {
-        voiceChannel.leave();
+      const dispatcher = connection.playFile(`./effects/teamspeak/${play}`);
+
+      dispatcher.on('end', end => {
+        message.member.voiceChannel.leave();
       });
     })
   }

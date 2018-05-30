9848d502319aea729bc56d7d2dfad954e89913d5
shadowolf <shadowolfbusiness@gmail.com>
2018-05-30 19:41:09 +1000

    Added startup command.

---
+++ commands/startup.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js');
+
+module.exports.run = async (client, message, args) => {
+  const voiceChannel = message.member.voiceChannel;
+
+  if (!message.member.voiceChannel) {
+    message.channel.send('Lol, I am not in a voice channel...');
+  } else {
+    voiceChannel.join().then(connection => {
+      const dispatcher = connection.playFile('./effects/startup.mp3');
+
+      dispatcher.on("end", end => {
+        voiceChannel.leave();
+      });
+    })
+  }
+};
+
+module.exports.help = {
+  name: 'startup'
+};

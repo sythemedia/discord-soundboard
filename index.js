2ff0e073f18f4fed37b4388eeef840924db37e30
shadowolf <shadowolfbusiness@gmail.com>
2018-05-31 20:07:45 +1000

    Added user not in voice channel error.

--- index.js
+++ index.js
@@ -47,6 +47,14 @@ client.on('message', message => {
   if (message.author.bot) return;
   if (message.channel.type === "dm") return;
 
+  if (!message.member.voiceChannel) {
+    let embed = new Discord.RichEmbed()
+    .setTitle('User is not in channel')
+    .setColor(config.red)
+    .setDescription(`${message.author}, you are not not in a voice channel dummy..`);
+    message.channel.send(embed);
+  };
+
   let prefix = config.prefix;
   let messageArray = message.content.split(" ");
   let cmd = messageArray[0];
@@ -58,4 +66,3 @@ client.on('message', message => {
 });
 
 client.login(process.env.BOT_TOKEN);
-//client.login();

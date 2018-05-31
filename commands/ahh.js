7a7c93ab15cc009c25e57cb70c7991cfa3193787
shadowolf <shadowolfbusiness@gmail.com>
2018-05-31 20:26:51 +1000

    Fixing error.

--- commands/ahh.js
+++ commands/ahh.js
@@ -1,7 +1,14 @@
 const Discord = require('discord.js');
 
 module.exports.run = async (client, message, args) => {
-  if (message.member.voiceChannel) {
+
+  if (!message.member.voiceChannel) {
+    let embed = new Discord.RichEmbed()
+    .setTitle('User is not in channel!')
+    .setColor(config.red)
+    .setDescription(`${message.author}, you are not not in a voice channel dummy..`);
+    message.channel.send(embed);
+  } else {
     message.member.voiceChannel.join().then(connection => {
       console.log(`[${message.guild}] ${message.author.username} has issues the ${module.exports.help.name} command.`)
       const dispatcher = connection.playFile('./effects/ahh.mp3');
--- index.js
+++ index.js
@@ -41,14 +41,6 @@ client.on('message', message => {
   if (message.author.bot) return;
   if (message.channel.type === 'dm') return;
 
-  if (!message.member.voiceChannel) {
-    let embed = new Discord.RichEmbed()
-    .setTitle('User is not in channel!')
-    .setColor(config.red)
-    .setDescription(`${message.author}, you are not not in a voice channel dummy..`);
-    message.channel.send(embed);
-  };
-
   let prefix = config.prefix;
   let messageArray = message.content.split(" ");
   let cmd = messageArray[0];

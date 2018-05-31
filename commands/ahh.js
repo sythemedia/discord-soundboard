00df1d287345497f3ec577a4eed77da4be3abefb
shadowolf <shadowolfbusiness@gmail.com>
2018-05-31 20:25:24 +1000

    Attempt to display name.

--- commands/ahh.js
+++ commands/ahh.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 module.exports.run = async (client, message, args) => {
   if (message.member.voiceChannel) {
     message.member.voiceChannel.join().then(connection => {
-      console.log(`[${message.guild}] ${message.author} has issues the ${module.exports.help.name} command.`)
+      console.log(`[${message.guild}] ${message.author.username} has issues the ${module.exports.help.name} command.`)
       const dispatcher = connection.playFile('./effects/ahh.mp3');
 
       dispatcher.on('end', end => {

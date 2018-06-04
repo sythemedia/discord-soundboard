6a9f2597b404e72861eeceb99a61871fc9216bd6
shadowolf <shadowolfbusiness@gmail.com>
2018-06-04 19:22:33 +1000

    Added comma.

--- util/errors.js
+++ util/errors.js
@@ -1,11 +1,10 @@
 const Discord = require('discord.js');
 const config = require('../config.json');
-const errors = require('../util/errors.js');
 
 module.exports.userNotInChannel = (message) => {
   let embed = new Discord.RichEmbed()
   .setTitle('User is not in channel!')
   .setColor(config.red)
-  .setDescription(`${message.author}, you are not not in a voice channel dummy..`);
+  .setDescription(`${message.author}, you are not not in a voice channel, dummy..`);
   message.channel.send(embed);
 };
